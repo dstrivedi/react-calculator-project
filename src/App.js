bc0e1fb8126fa6ec1f670b031f79da1697643739
dstrivedi <drashti1118@gmail.com>
2020-11-24 13:10:57 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,5 +2,34 @@ import React from "react";
 import styles from "./style.module.css";
 
 export default function App() {
-  return <div className={styles.app} />;
+  return (
+    <div className={styles.app}>
+      <div className={styles.buttons}>
+        <div>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>+</button>
+        </div>
+        <div>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>-</button>
+        </div>
+        <div>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>*</button>
+        </div>
+        <div>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>/</button>
+        </div>
+      </div>
+    </div>
+  );
 }
